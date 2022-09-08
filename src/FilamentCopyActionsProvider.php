69bf613831ffc21824234fcfa2503ca3851f7faa
Danilo Andrade <mandrade.danilo@gmail.com>
2022-09-08 17:56:38 -0300

    fix: remove unused imports

--- src/FilamentCopyActionsProvider.php
+++ src/FilamentCopyActionsProvider.php
@@ -5,12 +5,8 @@ namespace Webbingbrasil\FilamentCopyActions;
 use Filament\Facades\Filament;
 use Illuminate\Support\HtmlString;
 use Webbingbrasil\FilamentCopyActions\Forms\Actions\CopyAction;
-use function Filament\get_asset_id;
-use Filament\Navigation\UserMenuItem;
 use Filament\PluginServiceProvider;
-use Illuminate\Support\Facades\Config;
 use Spatie\LaravelPackageTools\Package;
-use Webbingbrasil\FilamentJetstreamTheme\Pages\Profile;
 
 class FilamentCopyActionsProvider extends PluginServiceProvider
 {
