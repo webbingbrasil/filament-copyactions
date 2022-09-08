48e494f8ac2f510fe2721d7b0f287ecbd17098d4
Danilo Andrade <mandrade.danilo@gmail.com>
2022-09-08 10:58:55 -0300

    fix: add clipboard eventlister using render hook

--- src/FilamentCopyActionsProvider.php
+++ src/FilamentCopyActionsProvider.php
@@ -3,6 +3,7 @@
 namespace Webbingbrasil\FilamentCopyActions;
 
 use Filament\Facades\Filament;
+use Illuminate\Support\HtmlString;
 use function Filament\get_asset_id;
 use Filament\Navigation\UserMenuItem;
 use Filament\PluginServiceProvider;
@@ -17,6 +18,16 @@ class FilamentCopyActionsProvider extends PluginServiceProvider
         $package
             ->name('filament-copyactions')
             ->hasViews();
+    }
 
+    public function packageBooted(): void
+    {
+        Filament::registerRenderHook('scripts.end', fn () => new HtmlString("
+            <script>
+                document.addEventListener('clipboard', function (e) {
+                    window.navigator.clipboard.writeText(e.detail);
+                });
+            </script>
+        "));
     }
 }
