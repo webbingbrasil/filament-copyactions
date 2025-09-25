3a8375a7f622d0678d8fdf205613e1962bca3a1d
Danilo Andrade <mandrade.danilo@gmail.com>
2025-09-25 15:02:43 -0300

    fix: remove has views #39

--- src/FilamentCopyActionsProvider.php
+++ src/FilamentCopyActionsProvider.php
@@ -9,8 +9,6 @@ class FilamentCopyActionsProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        $package
-            ->name('filament-copyactions')
-            ->hasViews();
+        $package->name('filament-copyactions');
     }
 }
